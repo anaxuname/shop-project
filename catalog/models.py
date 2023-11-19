9aa265adf0b909b3ae8832ce81ff77d6a4d506e7
Anaxuname <anaxuname@gmail.com>
2023-11-19 23:15:21 +0300

    new filter for admin and changed models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+NULLABLE = {'blank': True, 'null': True}
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
@@ -12,7 +13,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='наименование')
     description = models.CharField(max_length=150, verbose_name='описание')
-    image_preview = models.ImageField(upload_to='products/', verbose_name='изображение')
+    image_preview = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
     purchase_price = models.IntegerField(verbose_name='цена за покупку')
     data_create = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
