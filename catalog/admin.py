3907d7940c2d64579b5c95d73bb2de58a556018d
Anaxuname <anaxuname@gmail.com>
2023-11-19 21:38:56 +0300

    added Product and Category models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
+from catalog.models import Product, Category
+
+
 # Register your models here.
+@admin.register(Product)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
+
+
+@admin.register(Category)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description')
--- shop_project/settings.py
+++ shop_project/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'shop_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop-project',
+        'USER': 'postgres',
+        'PASSWORD': '1',
     }
 }
 
