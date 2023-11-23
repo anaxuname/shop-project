a721159f2f4391c99853235ca773114b85433d7a
Anaxuname <anaxuname@gmail.com>
2023-11-23 16:38:12 +0300

    new product template

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'index.html')
+    return render(request, 'catalog/index.html')
 
 
 def contacts_view(request):
-    return render(request, 'contacts.html')
+    return render(request, 'catalog/contacts.html')
+
+
+def products_view(request):
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Товары'
+    }
+    return render(request,'catalog/products.html', context)
