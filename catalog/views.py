c0f5371c8beb2b1bc486951a2992db5b5c9570d7
Anaxuname <anaxuname@gmail.com>
2023-11-25 11:03:34 +0300

    changed contact template

--- catalog/views.py
+++ catalog/views.py
@@ -5,10 +5,20 @@ from catalog.models import Product
 
 # Create your views here.
 def index_view(request):
-    return render(request, 'catalog/index.html')
+    product_list = Product.objects.all()
+    context = {
+        'object_list': product_list,
+        'title': 'Товары'
+    }
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts_view(request):
+    if request.method == 'POST':
+        name = request.Post.get('name')
+        email = request.Post.get('email')
+        message = request.Post.get('message')
+        print(f'{name} ({email}): {message}')
     return render(request, 'catalog/contacts.html')
 
 
@@ -16,6 +26,6 @@ def products_view(request):
     product_list = Product.objects.all()
     context = {
         'object_list': product_list,
-        'title': 'Товары'
+        'title': 'Товары2'
     }
     return render(request,'catalog/products.html', context)
